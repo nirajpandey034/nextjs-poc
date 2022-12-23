a06f7cc3539331107d6a7e02305e4f21a771bb63
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2022-12-23 23:10:40 +0530

    added some pages

---
+++ pages/User/login/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+function login() {
+  return (
+    <>
+    <Head>
+        <title>User Login</title>
+        <meta name="description" content="Login Page" />
+      </Head>
+      <p>Login page</p>
+    </>
+  )
+}
+
+export default login
---
+++ pages/User/register/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+function register() {
+  return (
+    <>
+      <Head>
+        <title>User Registration</title>
+        <meta name="description" content="Login Page" />
+      </Head>
+      <p>Registration page</p>
+    </>
+  );
+}
+
+export default register;
